fdd1d02d4f31e5fd19ff20dd08b26a34422ba8df
Mneach <mneachdev@gmail.com>
2023-05-07 11:45:03 +0700

    first commit

---
+++ OnlineJudge.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<string> vs;
+
+#define checkAnswer(x)  cout << "Answer : " << x << endl;
+
+int main()
+{
+    
+}    
---
+++ Socs.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<string.h>
+
+using namespace std;
+
+//Mneach
+
+//typedeff
+typedef long long ll;
+typedef unsigned long long ull;
+
+//scan and print
+#define si(x)   scanf("%d" , &x)
+#define sll(x)  scanf("%lld" , &x)
+#define ss(x)   scanf("%s" , &x)
+#define ssws(x) scanf("%[^\n]" , &x)
+#define pi(x)   printf("%d\n" , x)
+#define pll(x)  printf("%lld\n" , x)
+#define ps(x)   printf("%s\n" , x)
+#define pc(x)   printf("%c\n" , x)
+#define pitc(i,x) printf("Case #%d : %d\n" , i , x)
+#define pstc(i,x) printf("Case #%d : %s\n" , i , x)
+
+//loops
+#define FOR(i , a , b) for (int i = a; i < b; i++)
+#define FOR2(i , a , b) for (int i = a; i < b; i += 2)
+#define FOR3(i , a , b) for (int i = a; i < b; i += 3)
+#define FOREMIN(i , a , b) for (int i = a; i >= b; i--)
+#define FORED(i , a , b) for (int i = a; i <= b; i++)
+#define FORED2(i , a , b) for (int i = a; i <= b; i += 2)
+#define FORED3(i , a , b) for (int i = a; i <= b; i += 3)
+#define FOREU(i , a , b) for (int i = a; i >= b; i++)
+
+int main(){
+
+}   
