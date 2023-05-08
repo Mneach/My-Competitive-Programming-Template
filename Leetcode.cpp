7f1ec3076bbe6baad628b9e63f856ff154242c61
Mneach <mneachdev@gmail.com>
2023-05-08 09:30:30 +0700

    add Leetcode template

---
+++ Leetcode.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+    public : 
+        void problemName() {
+            cout << "Getting no message, is also a message..." << endl;
+        }
+};
+
+int main(){
+    Solution s;
+
+    s.problemName();
+    return 0;
+}
